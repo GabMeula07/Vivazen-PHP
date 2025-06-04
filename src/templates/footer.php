430183d1a97f1dd3b3925ddc9eb8780c6b6e12b2
GabMeula07 <gabrielqueirozmeula@gmail.com>
2025-06-03 22:20:12 -0300

    feat: :sparkles: creating new base templates

---
+++ src/templates/footer.php
@@ -0,0 +1,40 @@
+<footer>
+    <div class="container-footer">
+        <div class="info">
+            <h4>Unidade Brooklin</h4>
+            <p>Av. Doutor Elias Machado, 966</p>
+            <p>Telefone: (11) 9875-6937</p>
+            <p>Email: atendimento.brooklin@vivazen.com</p>
+        </div>
+        <div class="info">
+            <h4>Unidade Tatuapé</h4>
+            <p>Rua Travessa Roque Perez, 1795</p>
+            <p>Telefone: (11) 2185-2027</p>
+            <p>Email: atendimento.tatuape@vivazen.com</p>
+
+        </div>
+        <div class="info">
+            <h4>Unidade Capão</h4>
+            <p>Rua Laudelino Luz, 1918</p>
+            <p>Telefone: (11) 7718-6247</p>
+            <p>Email: atendimento.capao@vivazen.com</p>
+        </div>
+    </div>
+    <span>Todos os direitos reservados de Vivazen ©</span>
+</footer>
+
+<script>
+    function toggleMenu(button) {
+        const nav = document.querySelector(".mobile-nav");
+        button.classList.toggle("opened");
+        nav.classList.toggle("active");
+        button.setAttribute(
+            "aria-expanded",
+            button.classList.contains("opened")
+        );
+    }
+</script>
+<script src="./assets/js/fades.js"></script>
+</body>
+
+</html>
