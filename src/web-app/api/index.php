76448a40b9c3d0f4b968dee9647f1e0cc7ac0f94
GabMeula07 <gabrielqueirozmeula@gmail.com>
2025-06-08 23:17:41 -0300

    feat:

--- src/web-app/api/index.php
+++ src/web-app/api/index.php
@@ -3,16 +3,23 @@
 require_once "../core/database.php";
 require_once "../core/router.php";
 require_once "../controllers/test_database.php";
+require_once "../controllers/CustomerController.php";
+
+
 
 $router = new Router();
 
+$base_url = "/web-app/api";
 
-$router->add("get", "/web-app/api/test-conection", function () {
+$router->add("GET", "$base_url/test-conection/", function () {
     $testController = new TestConnectionController();
     $testController->test();
 });
 
-
-
+$router->add("POST", "$base_url/user/", function () {
+    $userController = new CustomerController();
+    echo $userController->CreateCustomer($_POST);
+    exit;
+});
 
 $router->dispath($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
